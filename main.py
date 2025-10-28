7335f0111b1fa2274587c35f09fa357dba972697
Haamid <haamidujillani@gmail.com>
2025-10-28 23:05:09 +0000

    Added randomly spawning apple on the grid

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 #.\.venv\Scripts\Activate.ps1
 import pygame
 import pprint
+import random
 
 WHITE = (200, 200, 200)
+BLACK = (10, 10, 10)
 SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 900
 CELL_SIZE = SCREEN_WIDTH//15
 player_cell = [7, 7]
 move_delay = 200   # milliseconds between moves
+
+
+
 def create_board():
     game_board = {}
     for x in range(1, 16):
@@ -18,11 +23,17 @@ def create_board():
 
 # print(pprint.pprint(game_board))
 
-def draw_board(screen):
+def draw_board(screen, apple_x, apple_y):
     for x in range(0, SCREEN_WIDTH, CELL_SIZE):
         for y in range(0, SCREEN_HEIGHT, CELL_SIZE):
             rect = pygame.Rect(x, y, CELL_SIZE, CELL_SIZE)
-            pygame.draw.rect(screen, WHITE, rect, 1)
+            
+            if x == apple_x * CELL_SIZE and y == apple_y * CELL_SIZE:
+                pygame.draw.circle(screen, BLACK, (CELL_SIZE * apple_y+CELL_SIZE/2, CELL_SIZE * apple_x + CELL_SIZE/2), 15)
+            else:
+                pygame.draw.rect(screen, WHITE, rect, 1)
+    
+
 
 def main():
 
@@ -37,7 +48,13 @@ def main():
 
     direction = ""
 
+    apple_x = random.randint(1, 15)
+    apple_y = random.randint(1, 15)
 
+    # If apple in player position set to default position
+    if apple_x == 7 and apple_y == 7:
+        apple_x = 2
+        apple_y = 2
 
     last_move_time = pygame.time.get_ticks()
     while running:
@@ -62,7 +79,7 @@ def main():
  
         # RENDER YOUR GAME HERE
 
-        draw_board(screen)
+        draw_board(screen, apple_x, apple_y)
         pygame.draw.circle(screen, "red", (CELL_SIZE * player_cell[0] + CELL_SIZE/2, CELL_SIZE * player_cell[1] + CELL_SIZE/2), CELL_SIZE//2 - 5)
         print(player_cell)
 
