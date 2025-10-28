2c3cc19e9b26745177e3a8d51479f2fd65b5085d
Haamid <haamidujillani@gmail.com>
2025-10-28 20:40:51 +0000

    Implemented snakes movement logic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #.\.venv\Scripts\Activate.ps1
 import pygame
+import pprint
 
 # pygame setup
 pygame.init()
@@ -7,7 +8,22 @@ screen = pygame.display.set_mode((900, 720))
 clock = pygame.time.Clock()
 running = True
 
+# Movement flags
+moving_up = False
+moving_right = False
+moving_down = False
+moving_left = False
+direction = ""
+
 dt = 0
+
+
+game_board = {}
+for x in range(1, 16):
+    for y in range(1, 16):
+        game_board[x, y] = " "
+    
+# print(pprint.pprint(game_board))
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
@@ -17,6 +33,15 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_s and direction != "UP":
+                direction = "DOWN"
+            elif event.key == pygame.K_w and direction != "DOWN":
+                direction = "UP"
+            elif event.key == pygame.K_a and direction != "RIGHT":
+                direction = "LEFT"
+            elif event.key == pygame.K_d and direction != "LEFT":
+                direction = "RIGHT"
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("green")
@@ -24,16 +49,20 @@ while running:
     # RENDER YOUR GAME HERE
     pygame.draw.circle(screen, "red", player_pos, 20)
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        while True:
-            player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
+
+    if direction == "UP":
+        player_pos.y -= 300 * dt
+
+    if direction == "DOWN":
         player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
+
+    if direction == "LEFT":
         player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
+
+    if direction == "RIGHT":
         player_pos.x += 300 * dt
+
+        
 
     # flip() the display to put your work on screen
     pygame.display.flip()
